4a8f54a92d9a804cc7dd59ff0e1021096ec2fa75
ViktorFAlex <viktor.f.alex@gmail.com>
2023-04-05 23:45:25 +0800

    fix rollbar

--- frontend/src/init.jsx
+++ frontend/src/init.jsx
@@ -1,5 +1,4 @@
 import { Suspense, lazy } from 'react';
-import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
 import i18next from 'i18next';
 import { I18nextProvider, initReactI18next } from 'react-i18next';
 import { Provider } from 'react-redux';
@@ -24,26 +23,17 @@ const init = async (socket) => {
     });
   const chatApi = buildChatApi(socket);
 
-  const rollbarConfig = {
-    accessToken: process.env.REACT_APP_NOT_SECRET_CODE,
-    environment: 'production',
-  };
-
   return (
     <Suspense fallback={<Preloader />}>
       <I18nextProvider i18n={i18n}>
-        <RollbarProvider config={rollbarConfig}>
-          <ErrorBoundary>
-            <Provider store={store}>
-              <AuthProvider>
-                <ChatApiProvider chatApi={chatApi}>
-                  <App />
-                </ChatApiProvider>
-              </AuthProvider>
-              <ToastContainer />
-            </Provider>
-          </ErrorBoundary>
-        </RollbarProvider>
+        <Provider store={store}>
+          <AuthProvider>
+            <ChatApiProvider chatApi={chatApi}>
+              <App />
+            </ChatApiProvider>
+          </AuthProvider>
+          <ToastContainer />
+        </Provider>
       </I18nextProvider>
     </Suspense>
   );
